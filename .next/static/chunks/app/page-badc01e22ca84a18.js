fb256f7bb9edf6130b21ebfa3eca42b923bdb144
sonah5009 <sonah5009@gmail.com>
2023-12-06 22:17:35 +0900

    .next push

---
+++ .next/static/chunks/app/page-badc01e22ca84a18.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7299:function(e,r,t){Promise.resolve().then(t.bind(t,7735))},7735:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return Home}});var n=t(7437),l=t(2265),s=t(4033);function Home(){let e=(0,s.useRouter)();return(0,l.useEffect)(()=>{let r=localStorage.getItem("user_key");r?e.push("/dashboard/friendlist"):e.push("/login")},[e]),(0,n.jsxs)("div",{className:"flex-col w-full h-full text-5xl font-bold flex-center bg-primary-yellow",children:[(0,n.jsx)("div",{className:"p-3 text-lg",children:(0,n.jsx)("div",{children:"( =①ω①=)"})}),(0,n.jsx)("div",{children:(0,n.jsx)("div",{className:"z-10",children:"CatCatTalk"})}),(0,n.jsx)("div",{className:"p-3 text-lg",children:(0,n.jsx)("div",{children:"V(=^･ω･^=)v"})})]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),l=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),o=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,c={},u=null,f=null;for(n in void 0!==t&&(u=""+t),void 0!==r.key&&(u=""+r.key),void 0!==r.ref&&(f=r.ref),r)s.call(r,n)&&!i.hasOwnProperty(n)&&(c[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===c[n]&&(c[n]=r[n]);return{$$typeof:l,type:e,key:u,ref:f,props:c,_owner:o.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)},4033:function(e,r,t){e.exports=t(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=7299)}),_N_E=e.O()}]);
